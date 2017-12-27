a51f6c2a7ed695b9d7eee738c6a97dec403e04ba
michaellee8 <ckmichael8@gmail.com>
2017-12-28 02:22:08 +0800

    Fix missing token build error

--- firebase-deploy.js
+++
@@ -1,9 +0,0 @@
-require("firebase-tools").deploy({
-    project: "ml8-visualizer",
-    token: process.env.FIREBASE_TOKEN,
-    cwd: "./"
-}).then(function(res) {
-    console.log(res, "deploy success")
-}).catch(function(err) {
-    console.log("deploy error", err)
-})
