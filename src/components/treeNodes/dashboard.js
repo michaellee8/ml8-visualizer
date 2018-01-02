84e14e122228dfd2328c2148aa2ad99b0e3e2a02
michaellee8 <ckmichael8@gmail.com>
2018-01-03 01:47:19 +0800

    Implment part of dashboard

--- src/components/treeNodes/dashboard.js
+++ src/components/treeNodes/dashboard.js
@@ -1,8 +1,23 @@
 import React from "react";
-class Dashboard extends React.Component {
+import AppBar from "react-appbar";
+import firebase from "firebase";
+import injectSheet from "react-jss";
+import UpIcon from "material-ui-icons/ChangeHistory"
+import EditIcon from "material-ui-icons/ModeEdit"
+
+type Props = { nodeId: string, userId: string, classes: any };
+type State = {};
+
+class Dashboard extends React.Component<Props, State> {
   render() {
-    return;
+    return <div><div className={this.props.classes.appBar} >
+        <div className={this.props.classes.appBarButton}><UpIcon/></div>
+        <div className={this.props.classes.appBarButton}><EditIcon/>/div>
+      </div></div>;
   }
 }
 
-export default Dashboard;
+export default injectSheet({
+  appBar: { position: "fixed", width: "100%", minHeight: "50px" },
+  appBarButton:{width:"50%"}
+})(Dashboard);
