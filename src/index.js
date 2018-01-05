cd8e076c9d9277721390988030689e5926d1b0a2
michaellee8 <ckmichael8@gmail.com>
2018-01-05 16:12:38 +0800

    Configure PWA

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import { render } from "react-dom";
 import firebase from "firebase";
 import "firebase/firestore";
 import App from "./App";
-
+import registerServiceWorker from "./createServiceWorker";
+registerServiceWorker();
 firebase.initializeApp({
   apiKey: "AIzaSyDLZQ6-PFhiUQrZOuUQth_LAEJBBvnZOYA",
   authDomain: "ml8-visualizer.firebaseapp.com",
@@ -12,5 +13,5 @@ firebase.initializeApp({
   storageBucket: "ml8-visualizer.appspot.com",
   messagingSenderId: "752477759795"
 });
-
+firebase.firestore().enablePersistence();
 render(<App />, document.getElementById("root"));
